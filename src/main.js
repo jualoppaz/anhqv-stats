ba416f11f10e9d2df6fcc5995155fa43c4b07942
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-03-19 19:35:31 +0100

    Importación de librería de i18n en script principal

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import ElementUI from 'element-ui';
 import App from './App.vue';
 
 import 'element-ui/lib/theme-chalk/index.css';
+import i18n from './i18n';
 
 Vue.use(ElementUI);
+
 Vue.config.productionTip = false;
 
 new Vue({
+  i18n,
   render: (h) => h(App),
 }).$mount('#app');
