bd021e997b89cab19e56009b4ee54d46872fd1f3
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-03-19 21:27:11 +0100

    Implementación inicial de vista de inicio

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="home">
+    Bienvenido a la Web de la Comunidad más loca de la TV.
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {},
+  created() {},
+};
+</script>
+
+<style lang="scss">
+
+#home {
+
+}
+</style>
