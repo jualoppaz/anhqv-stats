065c8614dc7f66be6945e925ddda8df846c56ae9
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-03-19 21:27:01 +0100

    Implementación inicial de vista para los personajes

---
+++ src/views/Characters.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="characters">
+    Aquí puedes ver el detalle de todos y cada uno de los personajes que aparecen en la serie.
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Characters',
+  components: {},
+  created() {},
+};
+</script>
+
+<style lang="scss">
+
+#characters {
+
+}
+</style>
