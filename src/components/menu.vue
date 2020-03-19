b6eb3137c967cc26c7e428f0329c399068506ca7
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-03-19 21:32:27 +0100

    Implementación de enlaces para navegar desde los links del menú

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -1,32 +1,24 @@
 <template>
   <el-aside id="aside-menu">
-    <el-menu :default-openeds="['1', '3']">
+    <el-menu :default-openeds="defaultOpeneds">
       <el-menu-item index="1">
-        <i class="el-icon-user-solid"></i>
-        <span> {{charactersItemText}}</span>
+        <router-link
+          to="/">
+          <i class="el-icon-s-home"></i> {{homeItemText}}
+        </router-link>
       </el-menu-item>
-      <el-submenu index="2">
-        <template slot="title"><i class="el-icon-video-camera-solid"></i>Temporadas</template>
-        <el-menu-item index="2-1"><i class="el-icon-film"></i>1ª Temporada</el-menu-item>
-        <el-menu-item index="2-2"><i class="el-icon-film"></i>2ª Temporada</el-menu-item>
-        <el-menu-item index="2-3"><i class="el-icon-film"></i>3ª Temporada</el-menu-item>
-        <el-menu-item index="2-4"><i class="el-icon-film"></i>4ª Temporada</el-menu-item>
-        <el-menu-item index="2-5"><i class="el-icon-film"></i>5ª Temporada</el-menu-item>
-      </el-submenu>
+      <el-menu-item index="2">
+        <router-link to="/characters">
+          <i class="el-icon-user-solid"></i> {{charactersItemText}}
+        </router-link>
+      </el-menu-item>
       <el-submenu index="3">
-        <template slot="title"><i class="el-icon-setting"></i>Navigator Three</template>
-        <el-menu-item-group>
-          <template slot="title">Group 1</template>
-          <el-menu-item index="3-1">Option 1</el-menu-item>
-          <el-menu-item index="3-2">Option 2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="Group 2">
-          <el-menu-item index="3-3">Option 3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="3-4">
-          <template slot="title">Option 4</template>
-          <el-menu-item index="3-4-1">Option 4-1</el-menu-item>
-        </el-submenu>
+        <template slot="title"><i class="el-icon-video-camera-solid"></i>Temporadas</template>
+        <el-menu-item index="3-1"><i class="el-icon-film"></i>1ª Temporada</el-menu-item>
+        <el-menu-item index="3-2"><i class="el-icon-film"></i>2ª Temporada</el-menu-item>
+        <el-menu-item index="3-3"><i class="el-icon-film"></i>3ª Temporada</el-menu-item>
+        <el-menu-item index="3-4"><i class="el-icon-film"></i>4ª Temporada</el-menu-item>
+        <el-menu-item index="3-5"><i class="el-icon-film"></i>5ª Temporada</el-menu-item>
       </el-submenu>
     </el-menu>
   </el-aside>
@@ -37,8 +29,14 @@
 export default {
   name: 'Menu',
   components: {},
+  data() {
+    return {
+      defaultOpeneds: ['3'],
+    };
+  },
   created() {
     this.charactersItemText = this.$t('MENU.CHARACTERS.TEXT');
+    this.homeItemText = this.$t('MENU.HOME.TEXT');
   },
 };
 </script>
@@ -53,6 +51,22 @@ export default {
     height: 100vh;
     -moz-user-select: none;
     -webkit-user-select: none;
+
+    li {
+      > a {
+
+        &.router-link-exact-active, &:focus{
+          color: #409EFF;
+
+          i {
+            color: #409EFF;
+          }
+        }
+        display: block;
+        //color: #303133;
+        text-decoration: none;
+      }
+    }
   }
 }
 </style>
