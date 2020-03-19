0bae35bc907558bf38f2c79b0a68affb8fad5e76
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-03-19 21:29:28 +0100

    Configuración de vue router en la aplicación

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import ElementUI from 'element-ui';
 import App from './App.vue';
 
 import 'element-ui/lib/theme-chalk/index.css';
 import i18n from './i18n';
+
+import router from './router';
+
+Vue.use(VueRouter);
 
 Vue.use(ElementUI);
 
@@ -11,5 +16,6 @@ Vue.config.productionTip = false;
 
 new Vue({
   i18n,
+  router,
   render: (h) => h(App),
 }).$mount('#app');
