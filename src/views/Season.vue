666ce93172fb352b2e582e9137dc154b407346d8
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-03-19 21:27:23 +0100

    Implementación inicial de vista dinámica para una temporada

---
+++ src/views/Season.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="season">
+    Capítulos de la temporada {{seasonNumber}}.
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Season',
+  props: ['seasonNumber'],
+  components: {},
+  created() {},
+};
+</script>
+
+<style lang="scss">
+
+#season {
+
+}
+</style>
