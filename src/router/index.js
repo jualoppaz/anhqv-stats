57cf64d6d5e4eb23c47eb99bec325302d14670d4
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-03-19 21:31:53 +0100

    Implementación de fichero con rutas de la aplicación

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  base: '/',
+  routes: [
+    {
+      path: '/',
+      component: () => import('@/views/Home'),
+      children: [],
+    }, {
+      path: '/characters',
+      component: () => import('@/views/Characters'),
+      children: [],
+    },
+  ],
+});
