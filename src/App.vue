c7188a010d6c82090ae9a41f5353f964151607c5
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-03-20 19:52:23 +0100

    Ajustes en el layout de la aplicación

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <el-container>
+    <el-container id="layout">
       <Menu/>
 
       <el-container id="content">
@@ -11,11 +11,11 @@
         <el-main id="main">
           <router-view></router-view>
         </el-main>
+        <el-footer id="footer">
+          Juan Manuel López Pazos
+        </el-footer>
       </el-container>
     </el-container>
-    <el-footer id="footer">
-      Juan Manuel López Pazos
-    </el-footer>
   </div>
 </template>
 
@@ -37,25 +37,53 @@ body{
   margin: 0;
 
   #app {
-    #content {
-      border-left: 1px solid #e6e6e6;
+    #layout {
+      height:100vh;
 
-      #header{
-        width: 100%;
-        height: 200px !important;
-        text-align: center;
-        padding: 0;
+      #content {
+        border-left: 1px solid #e6e6e6;
 
-        img {
-          max-height: 100%;
+        #header{
+          width: 100%;
+          height: 200px !important;
+          text-align: center;
+          padding: 0;
+
+          img {
+            max-height: 100%;
+          }
+        }
+
+        #footer{
+          border-top: 1px solid #e6e6e6;
+          text-align: center;
+          padding: 20px;
         }
       }
     }
+  }
 
-    #footer{
-      border-top: 1px solid #e6e6e6;
-      text-align: center;
-      padding: 20px;
+  .el-menu--vertical {
+    ul{
+      &.el-menu--popup-right-start{
+        li {
+          padding: 0 !important;
+          > a {
+            color: #303133;
+            display: block;
+            text-decoration: none;
+            padding: 0 20px;
+
+            &.router-link-exact-active, &:focus{
+              color: #409EFF;
+
+              i {
+                color: #409EFF;
+              }
+            }
+          }
+        }
+      }
     }
   }
 }
