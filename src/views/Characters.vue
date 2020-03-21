fe6d4ed1a5c351e56ef0750e9cabb240de91d827
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-03-21 11:56:07 +0100

    Implementación inicial de la vista de personajes con el listado de los mismos

--- src/views/Characters.vue
+++ src/views/Characters.vue
@@ -1,6 +1,37 @@
 <template>
   <div id="characters">
-    Aquí puedes ver el detalle de todos y cada uno de los personajes que aparecen en la serie.
+    <p>
+      Aquí puedes ver el detalle de todos y cada uno de los personajes que aparecen en la serie.
+    </p>
+    <el-row id="characters-list" :gutter="50">
+      <el-col
+        class="character-col"
+        :xs="8"
+        :sm="6"
+        :md="6"
+        v-for="character in characters"
+        :key="character.id">
+        <el-card
+          class="character-card"
+          shadow="hover"
+          :body-style="{ padding: '0px' }">
+          <img v-bind:src="character.image" class="image">
+          <div
+            class="character-card-info"
+            style="padding: 14px;">
+            <span><b>{{character.nickname}}</b></span>
+            <div class="bottom clearfix">
+              <el-button
+                type="text"
+                class="button"
+                v-on:click="goToDetail(character)">
+                Detalle
+              </el-button>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -10,12 +41,84 @@ export default {
   name: 'Characters',
   components: {},
   created() {},
+  data() {
+    return {
+      characters: [
+        {
+          id: 1,
+          name: 'Mauricio',
+          surname: 'Hidalgo',
+          nickname: 'Mauri',
+          image: 'https://vignette.wikia.nocookie.net/seriesspain/images/a/a6/Mauri.jpg/revision/latest/window-crop/width/200/x-offset/0/y-offset/0/window-width/250/window-height/250?cb=20110104141719&path-prefix=es',
+        }, {
+          id: 2,
+          name: 'María Luisa',
+          surname: 'Benito Valbuena',
+          nickname: 'Marisa',
+          image: 'https://vignette.wikia.nocookie.net/seriesspain/images/c/c4/Marisa_Benito.jpg/revision/latest/top-crop/width/300/height/300?cb=20111126095454&path-prefix=es',
+        }, {
+          id: 3,
+          name: 'Mauricio 3',
+          surname: 'Hidalgo',
+          image: 'https://vignette.wikia.nocookie.net/seriesspain/images/a/a6/Mauri.jpg/revision/latest/window-crop/width/200/x-offset/0/y-offset/0/window-width/250/window-height/250?cb=20110104141719&path-prefix=es',
+        }, {
+          id: 4,
+          name: 'Mauricio 4',
+          surname: 'Hidalgo',
+          image: 'https://vignette.wikia.nocookie.net/seriesspain/images/a/a6/Mauri.jpg/revision/latest/window-crop/width/200/x-offset/0/y-offset/0/window-width/250/window-height/250?cb=20110104141719&path-prefix=es',
+        }, {
+          id: 5,
+          name: 'Mauricio',
+          surname: 'Hidalgo',
+          nickname: 'Mauri',
+          image: 'https://vignette.wikia.nocookie.net/seriesspain/images/a/a6/Mauri.jpg/revision/latest/window-crop/width/200/x-offset/0/y-offset/0/window-width/250/window-height/250?cb=20110104141719&path-prefix=es',
+        }, {
+          id: 6,
+          name: 'María Luisa',
+          surname: 'Benito Valbuena',
+          nickname: 'Marisa',
+          image: 'https://vignette.wikia.nocookie.net/seriesspain/images/c/c4/Marisa_Benito.jpg/revision/latest/top-crop/width/300/height/300?cb=20111126095454&path-prefix=es',
+        }, {
+          id: 7,
+          name: 'Mauricio 3',
+          surname: 'Hidalgo',
+          image: 'https://vignette.wikia.nocookie.net/seriesspain/images/a/a6/Mauri.jpg/revision/latest/window-crop/width/200/x-offset/0/y-offset/0/window-width/250/window-height/250?cb=20110104141719&path-prefix=es',
+        }, {
+          id: 8,
+          name: 'Mauricio 4',
+          surname: 'Hidalgo',
+          image: 'https://vignette.wikia.nocookie.net/seriesspain/images/a/a6/Mauri.jpg/revision/latest/window-crop/width/200/x-offset/0/y-offset/0/window-width/250/window-height/250?cb=20110104141719&path-prefix=es',
+        },
+      ],
+    };
+  },
+  methods: {
+    goToDetail(character) {
+      console.log(character.id);
+    },
+  },
 };
 </script>
 
 <style lang="scss">
 
 #characters {
+  #characters-list{
+    .character-col{
+      padding: 6px;
 
+      .character-card{
+        text-align: center;
+
+        .image{
+          width: 100%;
+          display: block;
+        }
+        .character-card-info{
+          text-align: left;
+        }
+      }
+    }
+  }
 }
 </style>
