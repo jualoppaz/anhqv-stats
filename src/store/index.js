72a4b69262038b9a417f9370492dc6517fa22e16
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-03-22 21:08:23 +0100

    Implementación de script principal para el Store de vuex

---
+++ src/store/index.js
@@ -0,0 +1,9 @@
+import Vuex from 'vuex';
+import Vue from 'vue';
+import modules from './modules';
+
+Vue.use(Vuex);
+export default new Vuex.Store({
+  modules,
+  strict: process.env.NODE_ENV !== 'production',
+});
