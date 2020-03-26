b81a96b2a9ad2e52fc82f6bafe3abd8deaf7569d
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-03-26 21:43:15 +0100

    #4 Implementación de títulos como atributos meta en los componentes haciendo uso de la librería i18n de forma directa

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
+import i18n from '../i18n';
+
 Vue.use(Router);
 
 export default new Router({
@@ -11,14 +13,23 @@ export default new Router({
       path: '/',
       component: () => import('@/views/Home'),
       children: [],
+      meta: {
+        title: () => i18n.t('VIEWS.HOME.TITLE'),
+      },
     }, {
       path: '/characters',
       component: () => import('@/views/Characters'),
       children: [],
+      meta: {
+        title: () => i18n.t('VIEWS.CHARACTERS.TITLE'),
+      },
     }, {
       path: '/seasons/:season_number',
       component: () => import('@/views/Season'),
       children: [],
+      meta: {
+        title: ({ route }) => i18n.t('VIEWS.SEASONS.DETAIL.TITLE', { number: route.params.season_number }),
+      },
     },
   ],
 });
