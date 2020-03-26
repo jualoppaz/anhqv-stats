bbcb59f8cedc418a3bcb48ad03d0d2353bd17ba2
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-03-26 20:51:01 +0100

    #4 Implementación de cabecera de la web en componente Header

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="header">
+    <h1>{{title}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  computed: {
+    title() {
+      return this.$route.meta.title && this.$route.meta.title(this.$route).toUpperCase();
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#header{
+  height: 100%;
+  border-bottom: 1px solid $color-border-gray;
+
+  h1 {
+    margin: 0;
+    line-height: 60px;
+    padding: 0 20px;
+  }
+}
+</style>
