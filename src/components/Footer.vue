8c9fa0122a48e44f298b2bbb14e592b9c64463be
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-03-27 17:33:28 +0100

    #2 Implementación del componente Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="footer">
+    Copyright © 2020 <a href="https://www.juanmanuellopezpazos.es" target="_blank"> 👨‍💻 Juan Manuel López Pazos</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+};
+</script>
+
+<style lang="scss" scoped>
+#footer{
+  border-top: 1px solid $color-border-gray;
+  text-align: center;
+
+  line-height: 59px;
+  margin: 0;
+
+  a {
+    text-decoration: none;
+    color: $color-text-black;
+
+    &:hover{
+      color: $color-text-blue;
+    }
+  }
+}
+</style>
