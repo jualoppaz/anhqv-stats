2d8e28a33bc19e7bce6ec75358b9839eacb2da06
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-04-01 19:07:47 +0200

    #13 Implementación de métodos de utilidad para determinar si nos encontramos en un dispositivo determinado

--- src/utils/funcs.js
+++ src/utils/funcs.js
@@ -1,7 +1,25 @@
+import constants from './constants';
+
 function slugToText(slug) {
   return slug.split('-').map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
 }
 
+function isMobile() {
+  return window.innerWidth < constants.DEVICES.TABLET.MIN_WIDTH;
+}
+
+function isTablet() {
+  return window.innerWidth >= constants.DEVICES.TABLET.MIN_WIDTH
+    && window.innerWidth < constants.DEVICES.DESKTOP.MIN_WIDTH;
+}
+
+function isDesktop() {
+  return window.innerWidth >= constants.DEVICES.DESKTOP.MIN_WIDTH;
+}
+
 export default {
   slugToText,
+  isMobile,
+  isTablet,
+  isDesktop,
 };
