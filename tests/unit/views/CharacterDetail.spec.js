80f27f2911fdb2a55562ed874fa860c5e5c98511
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-04-03 20:24:45 +0200

    #19 Implementación de tests para la vista de detalle de un personaje

--- tests/unit/views/CharacterDetail.spec.js
+++ tests/unit/views/CharacterDetail.spec.js
@@ -2,15 +2,16 @@ import { createLocalVue, shallowMount } from '@vue/test-utils';
 import VueRouter from 'vue-router';
 import Vuex from 'vuex';
 import ElementUI from 'element-ui';
-import Characters from '../../../src/views/Characters.vue';
+import CharacterDetail from '../../../src/views/CharacterDetail.vue';
 
 const initialWidth = window.innerWidth;
 
-describe('Characters.vue', () => {
+describe('CharacterDetail.vue', () => {
   let localVue;
   let router;
   let store;
   let actions;
+  let state;
   beforeAll(() => {
     window.innerWidth = initialWidth;
     localVue = createLocalVue();
@@ -18,9 +19,15 @@ describe('Characters.vue', () => {
 
     router = new VueRouter();
 
+    state = {
+      current: {
+        name: 'John Doe',
+      },
+    };
 
     actions = {
-      getAll: jest.fn(),
+      getBySlug: jest.fn(),
+      destroyCurrent: jest.fn(),
     };
 
     localVue.use(Vuex);
@@ -29,6 +36,7 @@ describe('Characters.vue', () => {
       modules: {
         characters: {
           namespaced: true,
+          state,
           actions,
         },
       },
@@ -41,58 +49,81 @@ describe('Characters.vue', () => {
     };
 
     // eslint-disable-next-line no-unused-vars
-    const wrapper = shallowMount(Characters, {
+    const wrapper = shallowMount(CharacterDetail, {
       localVue,
       router,
       store,
       mocks: {
         $t: () => {},
+        $route: {
+          params: {
+            slug: 'john-doe',
+          },
+        },
       },
       methods,
     });
 
     expect(methods.handleResize).toHaveBeenCalled();
-    expect(actions.getAll).toHaveBeenCalled();
+    expect(actions.getBySlug).toHaveBeenCalled();
+    expect(actions.getBySlug.mock.calls[0][1]).toEqual({
+      slug: 'john-doe',
+    });
   });
 
   describe('handleResize', () => {
-    it('gutter for mobile', () => {
-      const wrapper = shallowMount(Characters, {
+    it('avatarSize for mobile', () => {
+      const wrapper = shallowMount(CharacterDetail, {
         localVue,
         store,
         mocks: {
           $t: () => {},
+          $route: {
+            params: {
+              slug: 'john-doe',
+            },
+          },
         },
         stubs: ['router-link', 'router-view'],
       });
 
       window.innerWidth = 767;
       wrapper.vm.handleResize();
-      expect(wrapper.vm.gutter).toEqual(20);
+      expect(wrapper.vm.avatarSize).toEqual(100);
     });
 
-    it('gutter for desktop', () => {
-      const wrapper = shallowMount(Characters, {
+    it('avatarSize for desktop', () => {
+      const wrapper = shallowMount(CharacterDetail, {
         localVue,
         store,
         mocks: {
           $t: () => {},
+          $route: {
+            params: {
+              slug: 'john-doe',
+            },
+          },
         },
         stubs: ['router-link', 'router-view'],
       });
 
       window.innerWidth = 800;
       wrapper.vm.handleResize();
-      expect(wrapper.vm.gutter).toEqual(50);
+      expect(wrapper.vm.avatarSize).toEqual(250);
     });
   });
 
   it('check beforeDestroy function is called', () => {
-    const wrapper = shallowMount(Characters, {
+    const wrapper = shallowMount(CharacterDetail, {
       localVue,
       store,
       mocks: {
         $t: () => {},
+        $route: {
+          params: {
+            slug: 'john-doe',
+          },
+        },
       },
       stubs: ['router-link', 'router-view'],
     });
