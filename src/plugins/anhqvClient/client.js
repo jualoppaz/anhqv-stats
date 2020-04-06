3b78bc991822cc05daf9a84464f72d30736cfa02
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-04-06 18:21:49 +0200

    #16 Implementación de método en el cliente para obtener el listado de capítulos de una temporada

--- src/plugins/anhqvClient/client.js
+++ src/plugins/anhqvClient/client.js
@@ -9,18 +9,40 @@ function AnhqvClient({
  */
 
 const CHARACTERS = 'characters';
+const CHAPTERS = 'chapters';
 
 AnhqvClient.prototype.setBaseURL = function setBaseURL(baseURL) {
   this.restClient.defaults.baseURL = baseURL;
 };
 
+/**
+ * Método que sirve para obtener el listado de personajes.
+ *
+ * @author jualoppaz
+ */
 AnhqvClient.prototype.getCharacters = function getCharacters() {
   return this.restClient.get(`/${CHARACTERS}`)
     .then((response) => response.data);
 };
 
+/**
+ * Método que sirve para obtener el detalle de un personaje dado su slug.
+ *
+ * @author jualoppaz
+ */
 AnhqvClient.prototype.getCharacterBySlug = function getCharacterBySlug(slug) {
   return this.restClient.get(`/${CHARACTERS}/${slug}`)
+    .then((response) => response.data);
+};
+
+/**
+ * Método que sirve para obtener el listado de capítulos que cumplan
+ * con los criterios proporcionados.
+ *
+ * @author jualoppaz
+ */
+AnhqvClient.prototype.getChapters = function getChapters(params) {
+  return this.restClient.get(`/${CHAPTERS}`, { params })
     .then((response) => response.data);
 };
 
