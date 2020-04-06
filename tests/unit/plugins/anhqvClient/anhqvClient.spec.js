9fe55047eaef7ae13c63105842515d8aeb952ab1
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-04-06 19:30:08 +0200

    #16 Implementación de test unitario para el método getChapters del cliente

--- tests/unit/plugins/anhqvClient/anhqvClient.spec.js
+++ tests/unit/plugins/anhqvClient/anhqvClient.spec.js
@@ -66,4 +66,25 @@ describe('anhqvClient', () => {
       });
     });
   });
+
+  describe('chapters', () => {
+    describe('getChapters', () => {
+      it('check url is well formed', (done) => {
+        const expectedUrl = '/chapters';
+        const expectedParams = {
+          season: '0',
+        };
+
+        mockClient.onGet().reply(200, {});
+
+        anhqvClient.getChapters(expectedParams)
+          .finally(() => {
+            expect(mockClient.history.get.length).toBe(1);
+            expect(mockClient.history.get[0].url).toBe(expectedUrl);
+            expect(mockClient.history.get[0].params).toEqual(expectedParams);
+            done();
+          });
+      });
+    });
+  });
 });
