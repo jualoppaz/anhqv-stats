d930e84c563e0680202dc39215959363fed19eb5
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-04-07 14:12:19 +0200

    #17 Implementación de métodos en el store de capítulos para obtener el detalle de un capítulo

--- src/store/modules/chapters.js
+++ src/store/modules/chapters.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 
 const state = {
   all: [],
+  current: {},
 };
 
 const getters = {};
@@ -9,13 +10,23 @@ const getters = {};
 const actions = {
   getAll({ commit }, params) {
     return Vue.anhqvClient.getChapters(params)
-      .then((characters) => commit('setAll', characters.data));
+      .then((chapters) => commit('setAll', chapters.data));
+  },
+  getBySlug({ commit }, { slug }) {
+    return Vue.anhqvClient.getChapterBySlug(slug)
+      .then((chapter) => commit('setCurrent', chapter));
+  },
+  destroyCurrent({ commit }) {
+    return commit('setCurrent', {});
   },
 };
 
 const mutations = {
-  setAll(state, characters) {
-    Vue.set(state, 'all', characters);
+  setAll(state, chapters) {
+    Vue.set(state, 'all', chapters);
+  },
+  setCurrent(state, chapter) {
+    Vue.set(state, 'current', chapter);
   },
 };
 
