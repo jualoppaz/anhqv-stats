bef95e865774df795ed39e8236e26bafd51010ec
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-04-07 14:37:33 +0200

    #17 Implementación de tests unitarios para el store de capítulos

--- tests/unit/store/chapters.spec.js
+++ tests/unit/store/chapters.spec.js
@@ -37,6 +37,38 @@ describe('store/chapters.js', () => {
           });
       });
     });
+
+    describe('getBySlug', () => {
+      it('it should call getChapterBySlug method and setCurrent mutation', (done) => {
+        const expectedChapter = {
+          name: 'Érase un X',
+        };
+
+        const slug = '0x01';
+
+        const commit = jest.fn();
+
+        Vue.anhqvClient.getChapterBySlug = jest.fn().mockResolvedValue(expectedChapter);
+
+        actions.getBySlug({ commit }, { slug })
+          .then(() => {
+            expect(Vue.anhqvClient.getChapterBySlug).toHaveBeenCalledWith(slug);
+
+            expect(commit).toHaveBeenCalledWith('setCurrent', expectedChapter);
+            done();
+          });
+      });
+    });
+
+    describe('destroyCurrent', () => {
+      it('it should call setCurrent mutation', () => {
+        const commit = jest.fn();
+
+        actions.destroyCurrent({ commit });
+
+        expect(commit).toHaveBeenCalledWith('setCurrent', {});
+      });
+    });
   });
 
   describe('mutations', () => {
@@ -53,6 +85,18 @@ describe('store/chapters.js', () => {
         mutations.setAll(state, chapters);
 
         expect(state.all).toEqual(chapters);
+      });
+    });
+
+    describe('setCurrent', () => {
+      it('it should modify current state property', () => {
+        const chapter = {
+          name: 'Érase un X',
+        };
+
+        mutations.setCurrent(state, chapter);
+
+        expect(state.current).toEqual(chapter);
       });
     });
   });
