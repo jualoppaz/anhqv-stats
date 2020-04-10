1100dbb8d093747ef77b45883eae9f76ee57ae83
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-04-10 17:55:53 +0200

    #27 Inclusión de anuncio en la página de inicio

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,17 @@
 <template>
   <div id="home">
+    <el-row>
+      <el-col>
+        <ins
+          class="adsbygoogle"
+          style="display:block"
+          data-ad-client="ca-pub-2309187828170787"
+          data-ad-slot="4044329554"
+          data-ad-format="auto"
+          data-full-width-responsive="true">
+        </ins>
+      </el-col>
+    </el-row>
     <p>
       Bienvenido a la Web de la Comunidad más loca de la TV.
     </p>
