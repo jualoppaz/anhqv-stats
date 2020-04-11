b42b70fcfe0d0193462b1774c42fd4be161e059e
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-04-11 13:25:11 +0200

    #28 Ajustes en el formato del anuncio para que sea fluido en el espacio disponible

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,5 @@
 <template>
   <div id="home">
-    <el-row>
-      <el-col>
-        <Adsense
-          data-ad-client="ca-pub-2309187828170787"
-          data-ad-slot="4044329554"
-          data-full-width-responsive="true">
-        </Adsense>
-      </el-col>
-    </el-row>
     <p>
       Bienvenido a la Web de la Comunidad más loca de la TV.
     </p>
