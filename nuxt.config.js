8e5cde6e70d2f92e51fca604af0c0edcf11dd579
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-04-20 21:43:26 +0200

    Importación de banner de moneytizer desde la configuración de nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,6 +23,11 @@ export default {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
     ],
+    script: [
+      {
+        src: '/js/moneytizer-banner.js', mode: 'client',
+      },
+    ],
   },
   /*
   ** Customize the progress-bar color
