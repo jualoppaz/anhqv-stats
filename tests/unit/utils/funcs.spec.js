e01fbb7a821a89e9c7e4ef6f52fb6c3907fdd7ba
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-05-04 23:23:09 +0200

    #75 Implementación de test unitario para el método getCommonMetas

--- tests/unit/utils/funcs.spec.js
+++ tests/unit/utils/funcs.spec.js
@@ -75,4 +75,68 @@ describe('utils/funcs', () => {
       expect(text).toBe('1x01');
     });
   });
+
+  describe('getCommonMetas', () => {
+    it('it should return all common metas', () => {
+      const seoConfig = {
+        title: 'Page title',
+        description: 'Page description',
+        canonical_url: 'Page canonical url',
+        og_title: 'OpenGraph title',
+        og_type: 'OpenGraph type',
+        og_image: 'OpenGraph image',
+        og_url: 'OpenGraph url',
+        og_description: 'OpenGraph description',
+        twitter_site: 'Twitter site',
+        twitter_card: 'Twitter card',
+      };
+
+      const expectedData = {
+        link: [
+          {
+            href: 'Page canonical url',
+            rel: 'canonical',
+          },
+        ],
+        meta: [
+          {
+            content: 'Page description',
+            hid: 'description',
+            name: 'description',
+          }, {
+            content: 'OpenGraph title',
+            hid: 'og:title',
+            property: 'og:title',
+          }, {
+            content: 'OpenGraph type',
+            hid: 'og:type',
+            property: 'og:type',
+          }, {
+            content: 'OpenGraph image',
+            hid: 'og:image',
+            property: 'og:image',
+          }, {
+            content: 'OpenGraph url',
+            hid: 'og:url',
+            property: 'og:url',
+          }, {
+            content: 'OpenGraph description',
+            hid: 'og:description',
+            property: 'og:description',
+          }, {
+            content: 'Twitter site',
+            hid: 'twitter:site',
+            name: 'twitter:site',
+          }, {
+            content: 'Twitter card',
+            hid: 'twitter:card',
+            name: 'twitter:card',
+          },
+        ],
+        title: 'Page title',
+      };
+
+      expect(utils.getCommonMetas(seoConfig)).toEqual(expectedData);
+    });
+  });
 });
