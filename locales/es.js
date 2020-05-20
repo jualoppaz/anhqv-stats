c2b128fd55eaf1e32a09ac938f86ba816bc18d80
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-05-20 17:29:20 +0200

    #89 Definición de literal con el title del enlace a mi web personal

--- locales/es.js
+++ locales/es.js
@@ -187,4 +187,9 @@ export default
       TEXT: '<p>En esta página se muestra la documentación Swagger de la API REST de la web <a href="http://www.anhqv-stats.es">ANHQV STATS</a>. Decir que se trata de una API <strong>NO OFICIAL</strong> de la serie Aquí No Hay Quien Viva. En ella podrás consultar información sobre: <strong>personajes</strong>, <strong>actores</strong>, <strong>capítulos</strong>, <strong>escenas</strong>... y hasta <strong>diálogos</strong>.</p>',
     },
   },
+  FOOTER: {
+    AUTHOR: {
+      TITLE: 'Página web personal de Juan Manuel López Pazos',
+    },
+  },
 };
