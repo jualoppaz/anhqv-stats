7a42322e116f27b17c3d012cbecf37f105d2c420
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-05-29 22:53:27 +0200

    Definición de literal para el banner de la página de documentación de la API

--- locales/es.js
+++ locales/es.js
@@ -193,6 +193,7 @@ export default
       },
     },
     API_DOC: {
+      TITLE: 'API REST',
       TEXT: '<p>En esta página se muestra la documentación Swagger de la API REST de la web <a href="http://www.anhqv-stats.es">ANHQV STATS</a>. Decir que se trata de una API <strong>NO OFICIAL</strong> de la serie Aquí No Hay Quien Viva. En ella podrás consultar información sobre: <strong>personajes</strong>, <strong>actores</strong>, <strong>capítulos</strong>, <strong>escenas</strong>... y hasta <strong>diálogos</strong>.</p>',
     },
   },
